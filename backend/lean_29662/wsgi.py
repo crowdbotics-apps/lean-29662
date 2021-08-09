5e49cbd9ef911e2f53476a63cf0b7c58d3ee5d58
Crowdbotics <team@crowdbotics.com>
2021-08-09 13:32:47 +0000

    Update models

--- backend/lean_29662/wsgi.py
+++ backend/lean_29662/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lean_29662.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lean_29662.settings")
 
 application = get_wsgi_application()
